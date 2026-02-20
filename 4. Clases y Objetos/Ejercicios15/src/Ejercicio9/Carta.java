dda8e2fe34fa6cf706ac7bfda545f34d36d38b9a
Vicky <vikiyumi@hotmail.com>
2026-02-20 12:50:08 +0100

    Cosas nuevas

--- 4. Clases y Objetos/Ejercicios15/src/Ejercicio9/Carta.java
+++ 4. Clases y Objetos/Ejercicios15/src/Ejercicio9/Carta.java
@@ -1,5 +1,7 @@
 package Ejercicio9;
 
+import java.util.Objects;
+
 public class Carta {
 	private String apellido;
 	private String nombre;
@@ -12,6 +14,36 @@ public class Carta {
 		this.nombre = nombre;
 		this.ciudad = ciudad;
 		this.pais = pais;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellido);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carta other = (Carta) obj;
+		return Objects.equals(apellido, other.apellido);
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 	
 	
--- 4. Clases y Objetos/Ejercicios15/src/Ejercicio9/Main.java
+++ 4. Clases y Objetos/Ejercicios15/src/Ejercicio9/Main.java
@@ -1,6 +1,7 @@
 package Ejercicio9;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -10,15 +11,39 @@ import Ejercicio3.Usuario;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int opc;
-		boolean correcto=false;
-		String apellido, nombre, pais, ciudad;
+		/*En una oficina de correos se quieren organizar las cartas que llegan. De cada
+		carta se guarda el apellido y nombre del destinatario, ciudad y país. Cada vez
+		que llega una carta a la oficina se coloca por orden alfabético en una colección
+		(elige la más adecuada).
+		Antes de guardar la carta en la LL anterior se comprueba que el país d de
+		destino es correcto, el país de destino sólo puede ser uno de 10 destinos
+		predeterminados al inicio del programa (usa la colección adecuada). Si el
+		destino no es correcto, la carta se coloca en el montón de excluidas (usa la
+		colección adecuada).
+		Las cartas se van extrayendo una a una de la colección inicial y se van
+		clasificando juntas las que tienen el mismo destino.
+		Mostrar en todo momento el contenido de todos los collection.
+		Hacer un menú que hasta que lo desee el usuario tenga las siguientes opciones:
+		1. Guardar carta ordenada. 2. Clasificar cartas. 3. Mostrar colecciones.*/
 		
+		int opc;
+		String apellido, nombre, pais, ciudad;
+		/**
+		 * Para guardar el objeto de la carta con todos sus atributos
+		 * */
 		LinkedList <Carta> cartas=new LinkedList<Carta>();
+		/**
+		 * Para guardar los 10 paises destino y poder comparar con el pais del objeto
+		 * */
 		HashSet <String> paises=new HashSet<String>();
-		ArrayList<Carta> c=new ArrayList<Carta>();
-		HashMap<String,ArrayList<>> 
+		
+		ArrayList<Carta> e=new ArrayList<Carta>();
+		/**
+		 * Para comparar el pais 
+		 * */
+		HashMap<String,ArrayList<Carta>> sacos=new HashMap<String,ArrayList<Carta>>();
+		
+		
 		
 		Scanner sc=new Scanner(System.in);
 		
@@ -45,7 +70,6 @@ public class Main {
 			
 			switch(opc) {
 			case 1:
-				do {
 					System.out.println("Introduce el apellido del destinatario: ");
 					apellido=sc.nextLine();
 					System.out.println("Introduce el nombre del destinatario: ");
@@ -54,9 +78,22 @@ public class Main {
 					ciudad=sc.nextLine();
 					System.out.println("Introduce el país del destinatario: ");
 					pais=sc.nextLine();
-				}while(!correcto);
+					//CREO UN NUEVO OBJETO CARTA
+					Carta carta=new Carta(apellido,nombre,ciudad,pais);
+					//AQUI COMPARO SI EL DATO PAIS DEL OBJETO, ESTA EN EL HASHSET
+					if (!paises.contains(pais)) {
+						e.add(carta); //SI NO ESTÁ LO AÑADO EN EL ARRAY DE EXCLUIDOS
+						System.out.println("El país destino no está dentro de los 10 países seleccionados");
+						System.out.println("La carta se pone en el montón de los excluidos.");
+					}
+					else {
+						int p=buscarPos(cartas, carta);
+						cartas.add(p,carta);
+						System.out.println("La carta se ha guardado correctamente");
+					}
 				break;
 			case 2:
+				
 				break;
 			case 3:
 				break;
@@ -68,6 +105,12 @@ public class Main {
 			}
 		}while(opc!=4);
 		
+	}
+	public static int buscarPos(LinkedList<Carta> cartas,Carta c) {
+		for(int i=0;i<cartas.size();i++)
+			if (cartas.get(i).getApellido().compareTo(c.getApellido())>0)
+				return i;
+		return cartas.size();
 	}
 
 }
