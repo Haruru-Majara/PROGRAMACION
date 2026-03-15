488e91f620ed11583de4c68a14d15e8acefb645f
Vicky <vikiyumi@hotmail.com>
2026-03-15 23:40:17 +0100

    Arreglando los nombres de las carpetas

---
+++ 4. Clases y Objetos/EjerciciosColecciones/src/Ej4_HashMap_Alumno/Main.java
@@ -0,0 +1,102 @@
+package Ej4_HashMap_Alumno;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int opc;
+		String nombre;
+		double nota;
+		
+		Scanner sc=new Scanner(System.in);
+		sc.useLocale(Locale.ENGLISH);
+		
+		HashMap <String, Double> alumnos = new HashMap<>();
+	
+	do {
+		System.out.println("                ");
+		System.out.println("------MENÚ------");
+		System.out.println("1.Alta");
+		System.out.println("2.Modificar nota");
+		System.out.println("3.Sustituir alumno");
+		System.out.println("4.Borrar");
+		System.out.println("5.Buscar");
+		System.out.println("6.Salir");
+		opc=sc.nextInt();
+		sc.nextLine();
+		switch(opc) {
+		case 1:
+			System.out.println("Introduce el nombre del alumno: ");
+			nombre=sc.nextLine();
+			if (alumnos.containsKey(nombre))
+				System.out.println("No se puede añadir, este alumno ya existe");
+			else {
+				System.out.println("Introduce nota");
+				nota=sc.nextDouble();
+				alumnos.put(nombre,nota);
+			}
+			break;
+		case 2:
+			System.out.println("Introduce el nombre del alumno: ");
+			nombre=sc.next();
+			if (!alumnos.containsKey(nombre))
+				System.out.println("No se puede modificar, este alumno no existe");
+			else {
+				System.out.println("Introduce nota nueva");
+				nota=sc.nextDouble();
+				alumnos.put(nombre,nota);
+			}
+			break;
+		case 3:
+			System.out.println("Introduce el nombre del alumno: ");
+			nombre=sc.next();
+			if (!alumnos.containsKey(nombre))
+				System.out.println("No se puede modificar, este alumno no existe");
+			else {
+				System.out.println("Introduce el nombre del nuevo alumno");
+				nombre=sc.nextLine();
+				System.out.println("Introduce la nota del nuevo alumno");
+				nota=sc.nextDouble();
+				alumnos.put(nombre,nota);
+				//alumnos.replace(nombre,nota);
+			}
+			break;
+		case 4:
+			System.out.println("Introduce el nombre del alumno: ");
+			nombre=sc.next();
+			if (alumnos.containsKey(nombre)) {
+				alumnos.remove(nombre);
+				System.out.println("Alumno borrado exitosamente");
+			}
+			else 
+				System.out.println("No se puede borrar un alumno que no existe");
+			break;
+			
+		case 5:
+			System.out.println("Introduce el nombre del alumno: ");
+			nombre=sc.next();
+			if (alumnos.containsKey(nombre))
+				System.out.println("Este alumno ya existe");
+			else 
+				System.out.println("No existe este alumno.");
+			break;
+		case 6:
+			System.out.println("Fin del programa.");
+			break;
+		default:
+			System.out.println("No existe esta opción.");
+		
+		}
+		
+		System.out.println("Los alumnos actualmente son: ");
+		for (String nombre1 : alumnos.keySet()) {
+            System.out.println(nombre1 + " con nota " + alumnos.get(nombre1));
+        }
+		
+		}while(opc!=6);
+	}
+}
---
+++ 4. Clases y Objetos/EjerciciosColecciones/src/Ej9_Colecciones_Oficina/Carta.java
@@ -0,0 +1,63 @@
+package Ej9_Colecciones_Oficina;
+
+import java.util.Objects;
+
+public class Carta {
+	private String apellido;
+	private String nombre;
+	private String ciudad;
+	private String pais;
+	
+	
+	public Carta(String apellido, String nombre, String ciudad, String pais) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+		this.ciudad = ciudad;
+		this.pais = pais;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellido);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carta other = (Carta) obj;
+		return Objects.equals(apellido, other.apellido);
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+
+	public String getPais() {
+		return pais;
+	}
+
+
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	
+	
+}
